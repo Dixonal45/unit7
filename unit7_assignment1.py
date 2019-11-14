ab7cfe7766c8e00f22943382cfb280b6d0719d29
allison <allison.dixon@ssfs.org>
2019-11-14 08:50:31 -0500

    finished unit 7 project

--- unit7_assignment1.py
+++ unit7_assignment1.py
@@ -1,35 +1,74 @@
 # created by Allison Dixon
-# last modified November 12, 2019
+# last modified November 14, 2019
 # unit 7 project
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 encoded_word = []
+decoded_word = []
 
 
 def shift():
-    user_shift = int(input("How many letters do you want to shift the alphabet?"))
+    """
+    This function shifts the alphabet however much the user wants.
+    :return: the new alphabet
+    """
+    user_shift = int(input("How many letters do you want to shift the alphabet (1-25)?"))
     first = alphabet[0:user_shift]
     last = alphabet[user_shift:]
     alphabet_two = last + first
     return alphabet_two
 
 
-def encode(new_alphabet):
+def encode():
+    """
+    This function takes a word or sentence from the user and encodes it using the shifted alphabet and prints it.
+    :return: nothing
+    """
+    new_alphabet = shift()
     user_word = input("What word do you want to encode?")
     user_word = user_word.lower()
+    final_encoded = ""
     for letter in user_word:
-        position = alphabet.index(letter)
-        position_final = new_alphabet[position]
-        encoded_word.append(position_final)
+        if letter not in alphabet:
+            encoded_word.append(letter)
+        else:
+            position = alphabet.index(letter)
+            position_final = new_alphabet[position]
+            encoded_word.append(position_final)
         final_encoded = "".join(encoded_word)
     print(final_encoded)
 
 
-def main():
+def decode():
+    """
+    This function takes an encoded word or sentence from the user and decodes it and prints it.
+    :return: nothing
+    """
     new_alphabet = shift()
-    user_input = input("Press 'e' to encode, 'd' to decode, or 'q' to quit.")
-    encode(new_alphabet)
+    user_decode_word = input("What word do you want to decode?")
+    user_decode_word = user_decode_word.lower()
+    final_decoded = ""
+    for letter in user_decode_word:
+        if letter not in alphabet:
+            decoded_word.append(letter)
+        else:
+            position = new_alphabet.index(letter)
+            position_final = alphabet[position]
+            decoded_word.append(position_final)
+        final_decoded = "".join(decoded_word)
+    print(final_decoded)
+
+
+def main():
+    while True:
+        user_input = input("Press 'e' to encode, 'd' to decode, or 'q' to quit.")
+        if user_input == "e":
+            encode()
+        elif user_input == "d":
+            decode()
+        elif user_input == "q":
+            print("Thanks anyways.")
+            break
 
 
 main()
-
